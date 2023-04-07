10b28673f8d42a392c0229bd50df0f6228df610c
okancaglar <okancaglar7@gmail.com>
2023-04-07 04:10:08 +0300

    solution

---
+++ MaxDepthOfBinaryTree.java
@@ -0,0 +1,9 @@
+public class MaxDepthOfBinaryTree {
+    public int maxDepth(TreeNode root){
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        return Math.max(left, right);
+    }
+}
